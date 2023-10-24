562d71637e26e485e7d79ca155fd6fc5de5da609
Ahmed Corhusic <acorhusic1@etf.unsa.ba>
2023-10-24 23:01:31 +0200

    Gotov zadatak

---
+++ src/Korisnik.java
@@ -0,0 +1,6 @@
+public class Korisnik extends Osoba {
+    private Racun racun;
+    public Korisnik(String ime, String prezime) {
+        super(ime, prezime);
+    }
+}
---
+++ src/Racun.java
@@ -0,0 +1,23 @@
+public class Racun {
+    private Long brojRacuna;
+    private Osoba korisnikRacuna;
+    private boolean odobrenjePrekoracenja;
+    private double stanjeRacuna;
+
+    public Racun(Long brojRacuna, Osoba korisnikRacuna) {
+        this.brojRacuna = brojRacuna;
+        this.korisnikRacuna = korisnikRacuna;
+    }
+
+    private boolean provjeriOdobrenjePrekoracenja() {
+        return odobrenjePrekoracenja;
+    }
+
+    public void izvrsiUplatu(double stanjeRacuna) {
+        this.stanjeRacuna = stanjeRacuna;
+    }
+
+    public void odobriPrekoracenje(double prekoracenje) {
+        odobrenjePrekoracenja = true;
+    }
+}
